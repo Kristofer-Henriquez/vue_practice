602e291dfb9d0f731f9b8d551f6d45991c8a58a2
Kristofer <kristoferhenriquez@gmail.com>
2020-08-02 11:55:59 -0500

    testing toggle button

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ var app = new Vue({
     message: "Hello from JavaScript!",
     yolo: "yolo",
     name: "Kris",
+    ShowInfo: false,
   },
   methods: {
     ChangeMessage: function() {
       console.log("changing message...");
       this.message = "hello from another dimension";
-    }
+    },
+    ToggleInfo: function() {
+      console.log("hiding info...");
+      this.ShowInfo = !this.ShowInfo;
+    },
   }
 });
